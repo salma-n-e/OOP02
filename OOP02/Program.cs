e59a4e554aa0e25bdcaa10b6843fee8885e5fd86
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-12 17:55:45 +0300

    Q 1

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace OOP02
+﻿using System.Reflection;
+
+namespace OOP02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            // Q 1 
+            // a)  struct is a value type, while class is a reference type.
+            // structs are stored on the stack, while classes are stored on the heap.
+            // Structs cannot have a default constructor, while classes can.
+            // Structs cannot inherit from other structs or classes, while classes can.
+            // Structs are generally used for small data structures that do not require inheritance or polymorphism,
+            // while classes are used for more complex data structures that require these features
+
+            // b) Inheritance & Reusability: They support OOP principles like inheritance and polymorphism,
+            // making the codebase scalable and maintainable.
+
+          //  Performance with Large Data: Passing a class (Reference Type) copies only its memory address,
+          //  whereas passing a struct (Value Type) copies the entire object,
+          //  which overheads memory when dealing with large data structures.
         }
     }
 }
