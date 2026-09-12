ae22ee2c45bc81adfd1b412d4a10340c1adfdaa8
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-12 23:35:08 +0300

    2. Create Three Shipment Types

---
+++ OOP02/StandardShipment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    public class StandardShipment : Shipment
+    {
+        public StandardShipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination)
+    : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+
+    }
+}
+
